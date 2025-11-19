2ddbbeb467a0e5f30cece4ff14b29c19680eef0e
robert thornton <robert.thornton92@gmail.com>
2025-11-19 13:46:24 +0200

    Moved from src to src/api

--- api/index.js
+++ api/index.js
@@ -2,13 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { createServer } from "@vendia/serverless-express";
-import router from './src/routers/index.js';
+import router from '../src/routers/index.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', router)
+// app.use('/', router)
+router.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
 
 export default createServer(app);
