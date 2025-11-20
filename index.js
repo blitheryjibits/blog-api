6433bc859452b0b7db037fc1196894089400e04b
robert thornton <robert.thornton92@gmail.com>
2025-11-20 12:13:59 +0200

    possibly sane again... if this works

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-// import express from 'express';
-// import { createServer } from "@vendia/serverless-express";
-// import router from './src/routers/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import { createServer } from "@vendia/serverless-express";
+import router from './src/routers/index.js';
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // in index.js or api entry
-// app.get('/favicon.ico', (req, res) => res.status(204).end());
-// app.get('/favicon.png', (req, res) => res.status(204).end());
+app.use('/', router)
 
-// app.use('/', router)
-// router.get('/ping', (req, res) => {
-//   res.send('API is running ...');
-// });
-
-export default createServer(app);
-
-// const handler = createServer(app);
-
-// export default function (req, res) {
-//   return handler(req, res);
-// }
-// export default function handler(req, res) { res.status(200).json({ ok: true }); }
+export default app;
