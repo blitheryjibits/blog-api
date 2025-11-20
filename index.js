18389a58ca1d11c0c559f57d53b9ebe7de1ae62a
robert thornton <robert.thornton92@gmail.com>
2025-11-20 09:23:56 +0200

    going crazy at this point

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// in index.js or api entry
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.get('/favicon.png', (req, res) => res.status(204).end());
+
 app.use('/', router)
 router.get('/ping', (req, res) => {
   res.send('API is running ...');
