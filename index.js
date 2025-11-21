76fa278b3eb65d328fad953c67f132a32a3ab0f8
robert thornton <robert.thornton92@gmail.com>
2025-11-21 08:56:23 +0200

    Removed deprecated create server function from vendia package and changed app to export default. This is the current approach to express serverless functions in Vercel.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import { createServer } from "@vendia/serverless-express";
 import router from './src/routers/index.js';
 
 const app = express();
