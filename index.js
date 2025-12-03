1c4f5a8067a6b0fbc5a7d3b3e41901a1e9ff6da2
robert thornton <robert.thornton92@gmail.com>
2025-12-03 08:53:04 +0200

    Changing cors settings... again

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const app = express();
 
 app.use(cors({
   origin: 'http://127.0.0.1:5173', // temporary frontend origin for testing.
-  credentials: false,
-  allowedHeaders: ['Content-Type','Authorization','Accept'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204
+
 }));
 
 app.options('*', cors()); // ensure preflight handled
