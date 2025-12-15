7face939449cf8d3709a0dd1496b24fd0c4d774a
robert thornton <robert.thornton92@gmail.com>
2025-12-15 13:22:19 +0200

    just keep swimming

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // ✅ Global CORS handler for Vercel serverless
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
 
