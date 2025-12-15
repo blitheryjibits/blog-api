cf6d60dce80058f3ad213fd76e5f09301a437baf
robert thornton <robert.thornton92@gmail.com>
2025-12-15 13:53:20 +0200

    more changes for vercel serverless functions

---
+++ api/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import router from '../src/routers/index.js';
+import serverless from 'serverless-http';
+
+const app = express();
+
+// ✅ Global CORS for Vercel serverless
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blog API!');
+});
+
+// ✅ Export serverless handler
+export default serverless(app);
