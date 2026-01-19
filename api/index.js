74b91aad238546f71071af981c0467bf8225d390
robert thornton <robert.thornton92@gmail.com>
2026-01-19 09:28:27 +0200

    Switch Hosting Service - changes made to allow serving from Render instead of Vercel.

--- api/index.js
+++ api/index.js
@@ -3,35 +3,21 @@ dotenv.config();
 
 import express from 'express';
 import router from '../src/routers/index.js';
-import serverless from 'serverless-http';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-
-// ✅ Global CORS for Vercel serverless
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  next();
-});
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
 
-
-
 app.get('/', (req, res) => {
   res.send('Welcome to the Blog API!');
 });
 
-// ✅ Export serverless handler
-export default serverless(app);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
+
