bf20d2855a11efc022c13ae61a69c3869f0441fd
robert thornton <robert.thornton92@gmail.com>
2026-01-24 11:06:32 +0200

    Removed - redundant files

--- src/generated/models.ts
+++ src/generated/models.ts
@@ -8,7 +8,7 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/Post.ts'
-export type * from './models/User.ts'
-export type * from './models/Comment.ts'
-export type * from './commonInputTypes.ts'
+export type * from './models/Post.js'
+export type * from './models/User.js'
+export type * from './models/Comment.js'
+export type * from './commonInputTypes.js'
